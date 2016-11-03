22f28e979edeb0c81244837de2527f0ebe5236b4
Katharine Amaral <katharineamaral@alu.ufc.br>
2016-11-02 22:54:13 -0200

    classe dados

--- Lista2-Questão4/src/Dados.java
+++ Lista2-Questão4/src/Dados.java
@@ -1,10 +1,58 @@
+import java.util.ArrayList;
 
-public class Dados {
+public class Dados implements IDados{
 	int valorA, valorB, valorC;
+	ArrayList<Observer> listaObservadores = new ArrayList<Observer>();
 	
 	public Dados(int a, int b, int c){
 		valorA = a;
 		valorB = b;
 		valorC = c;
 	}
+
+	public int getValorA() {
+		return valorA;
+	}
+
+	public void setValorA(int valorA) {
+		this.valorA = valorA;
+		notificarObservadores();
+	}
+
+	public int getValorB() {
+		return valorB;
+	}
+
+	public void setValorB(int valorB) {
+		this.valorB = valorB;
+		notificarObservadores();
+	}
+
+	public int getValorC() {
+		return valorC;
+	}
+
+	public void setValorC(int valorC) {
+		this.valorC = valorC;
+		notificarObservadores();
+	}
+
+	@Override
+	public void registrar(Observer o) {
+		listaObservadores.add(o);
+	}
+
+	@Override
+	public void desregistrar(Observer o) {
+		listaObservadores.remove(o);		
+	}
+
+	@Override
+	public void notificarObservadores() {
+		for(int i=0;i<listaObservadores.size();i++){
+			listaObservadores.get(i).update(valorA,valorB,valorC);
+		}	
+	}
+	
+	
 }
